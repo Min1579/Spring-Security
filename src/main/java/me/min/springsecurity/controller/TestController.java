e6b5db8a5ee409e863794fcb2f1e118bef108c4b
Min1579 <hsm012362@gmail.com>
2021-01-21 16:18:22 +0900

    feat: implemented sample controller

---
+++ src/main/java/me/min/springsecurity/controller/TestController.java
@@ -0,0 +1,15 @@
+package me.min.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello/{name}")
+    public String hello(@PathVariable String name) {
+        return "hello ".concat(name);
+    }
+
+}
