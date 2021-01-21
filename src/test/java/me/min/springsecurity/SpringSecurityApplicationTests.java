2b1a8dfef10ee93bf59e5d1dbe4270a25943f431
Min1579 <hsm012362@gmail.com>
2021-01-21 17:35:19 +0900

    feat: init data test

--- src/test/java/me/min/springsecurity/SpringSecurityApplicationTests.java
+++ src/test/java/me/min/springsecurity/SpringSecurityApplicationTests.java
@@ -1,13 +1,29 @@
 package me.min.springsecurity;
 
+import me.min.springsecurity.model.User;
+import me.min.springsecurity.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.annotation.DirtiesContext;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DirtiesContext
+@Profile("test")
 @SpringBootTest
 class SpringSecurityApplicationTests {
+    @Autowired
+    private UserRepository userRepository;
 
     @Test
-    void contextLoads() {
+    void initDataCheck() {
+        List<User> users = userRepository.findAll();
+        System.out.println(users);
+        assertThat(users.size()).isEqualTo(1);
+        assertThat(users.get(0).getUsername()).isEqualTo("min");
     }
-
 }
