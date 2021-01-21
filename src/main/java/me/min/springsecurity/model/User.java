422954d995d454352bff260d10f53a10dc0cd8e3
Min1579 <hsm012362@gmail.com>
2021-01-21 17:34:01 +0900

    feat: implemented user entity & repository

---
+++ src/main/java/me/min/springsecurity/model/User.java
@@ -0,0 +1,29 @@
+package me.min.springsecurity.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    @Column(name = "user_id")
+    private Long id;
+    private String username;
+    private String password;
+
+    @Builder
+    public User(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+}
